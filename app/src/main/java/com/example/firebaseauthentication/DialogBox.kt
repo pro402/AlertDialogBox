3b7325511ece238022278fa05ab0201bba9c63ba
pro402 <maitypro402@gmail.com>
2023-03-28 17:09:07 +0530

    changing Database structue

--- app/src/main/java/com/example/firebaseauthentication/DialogBox.kt
+++ app/src/main/java/com/example/firebaseauthentication/DialogBox.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.Toast
 import com.example.firebaseauthentication.databinding.ActivityDialogBoxBinding
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -14,6 +15,7 @@ class DialogBox : AppCompatActivity() {
     lateinit var binding: ActivityDialogBoxBinding
     lateinit var dialog: Dialog
     lateinit var database: DatabaseReference
+    private lateinit var userId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +30,19 @@ class DialogBox : AppCompatActivity() {
         val name = dialog.findViewById<TextInputEditText>(R.id.name)
         val phone = dialog.findViewById<TextInputEditText>(R.id.phonenumber)
 
+        // Get the current user's ID
+        userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+
         dialogView.findViewById<Button>(R.id.add)?.setOnClickListener {
             val n = name.text.toString()
             val p = phone.text.toString()
             val contacts = contacts(n,p)
-            database= FirebaseDatabase.getInstance().getReference("User/Contacts")
+
+            // Create a branch in the database for the current user
+            database = FirebaseDatabase.getInstance().getReference("User/Contacts(userUID)/$userId")
+
+            // Save the contact data to the current user's branch
             database.child(p).setValue(contacts).addOnSuccessListener {
                 Toast.makeText(this,"Successfully Registered contact ${n} and phone number ${p}", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener {
@@ -41,13 +51,10 @@ class DialogBox : AppCompatActivity() {
 
             name.setText("")
             phone.setText("")
-
         }
 
         binding.dialogButton.setOnClickListener {
             dialog.show()
         }
-
     }
 }
-
